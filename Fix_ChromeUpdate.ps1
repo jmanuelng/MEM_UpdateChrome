23058087e4168bc4e0fe94ffe13da0d1d83b9aab
Manuel Nieto <manuel.nieto@outlook.com>
2023-08-06 22:56:48 -0500

    v0.6 Updated function "Find-WingetPath"

--- Fix_ChromeUpdate.ps1
+++ Fix_ChromeUpdate.ps1
@@ -27,34 +27,97 @@ Write-Host `n`n
 #region Functions
 
 function Find-WingetPath {
-    # Define the possible locations for winget.exe
+    <#
+    .SYNOPSIS
+        Locates the winget.exe executable within a system.
+
+    .DESCRIPTION
+        Finds the path of the `winget.exe` executable on a Windows system. 
+        Aimed at finding Winget when main script is executed as SYSTEM, but will also work under USER
+        
+        Windows Package Manager (`winget`) is a command-line tool that facilitates the 
+        installation, upgrade, configuration, and removal of software packages. Identifying the 
+        exact path of `winget.exe` allows for execution (installations) under SYSTEM context.
+
+        METHOD
+        1. Defining Potential Paths:
+        - Specifies potential locations of `winget.exe`, considering:
+            - Standard Program Files directory (64-bit systems).
+            - 32-bit Program Files directory (32-bit applications on 64-bit systems).
+            - Local application data directory.
+            - Current user's local application data directory.
+        - Paths may utilize wildcards (*) for flexible directory naming, e.g., version-specific folder names.
+
+        2. Iterating Through Paths:
+        - Iterates over each potential location.
+        - Resolves paths containing wildcards to their actual path using `Resolve-Path`.
+        - For each valid location, uses `Get-ChildItem` to search for `winget.exe`.
+
+        3. Returning Results:
+        - If `winget.exe` is located, returns the full path to the executable.
+        - If not found in any location, outputs an error message and returns `$null`.
+
+    .EXAMPLE
+        $wingetLocation = Find-WingetPath
+        if ($wingetLocation) {
+            Write-Output "Winget found at: $wingetLocation"
+        } else {
+            Write-Error "Winget was not found on this system."
+        }
+
+    .NOTES
+        While this function is designed for robustness, it relies on current naming conventions and
+        structures used by the Windows Package Manager's installation. Future software updates may
+        necessitate adjustments to this function.
+
+    .DISCLAIMER
+        This function and script is provided as-is with no warranties or guarantees of any kind. 
+        Always test scripts and tools in a controlled environment before deploying them in a production setting.
+        
+        This function's design and robustness were enhanced with the assistance of ChatGPT, it's important to recognize that 
+        its guidance, like all automated tools, should be reviewed and tested within the specific context it's being 
+        applied. 
+
+    #>
+    # Define possible locations for winget.exe
     $possibleLocations = @(
-        "${env:ProgramFiles}\WindowsApps\Microsoft.DesktopAppInstaller_*\app\winget.exe",
-        "${env:ProgramFiles(x86)}\WindowsApps\Microsoft.DesktopAppInstaller_*\app\winget.exe",
+        "${env:ProgramFiles}\WindowsApps\Microsoft.DesktopAppInstaller*_x64__8wekyb3d8bbwe\winget.exe", 
+        "${env:ProgramFiles(x86)}\WindowsApps\Microsoft.DesktopAppInstaller*_8wekyb3d8bbwe\winget.exe",
         "${env:LOCALAPPDATA}\Microsoft\WindowsApps\winget.exe",
         "${env:USERPROFILE}\AppData\Local\Microsoft\WindowsApps\winget.exe"
     )
 
-    # Try to find winget.exe in the possible locations
+    # Iterate through the potential locations and return the path if found
     foreach ($location in $possibleLocations) {
         try {
-            # Get the items that match the current location
+            # Resolve path if it contains a wildcard
+            if ($location -like '*`**') {
+                $resolvedPaths = Resolve-Path $location -ErrorAction SilentlyContinue
+                # If the path is resolved, update the location for Get-ChildItem
+                if ($resolvedPaths) {
+                    $location = $resolvedPaths.Path
+                }
+                else {
+                    # If path couldn't be resolved, skip to the next iteration
+                    Write-Warning "Couldn't resolve path for: $location"
+                    continue
+                }
+            }
+            
+            # Try to find winget.exe using Get-ChildItem
             $items = Get-ChildItem -Path $location -ErrorAction Stop
-
-            # If an item is found, return the full path of winget.exe
             if ($items) {
-                $wingetPath = $items[0].FullName
-                return $wingetPath
+                Write-Host "Found Winget at: $items"
+                return $items[0].FullName
+                break
             }
         }
-        # If an error occurs, continue searching in the next location
         catch {
-            Write-Warning "Unable to search for winget.exe in the following location: $location"
+            Write-Warning "Couldn't search for winget.exe at: $location"
         }
     }
 
-    # If winget.exe is not found in any location, display an error message and return $null
-    Write-Error "Winget not found in any of the checked locations."
+    Write-Error "Winget wasn't located in any of the specified locations."
     return $null
 }
 
